d5c5dedf6a87f96cc225e031626d3a73697cec0c
Jack Anderson <jackanderson02@outlook.com>
2025-06-23 00:07:23 +0100

    wip: mystical metatables

--- lua/runner.lua
+++ lua/runner.lua
@@ -9,8 +9,42 @@ local Runner = {
 
 ---@class State
 local State = {
+  file = nil,
   cmd = nil,
 }
+
+-- Constructs a State object and sets its meta table to be the State table
+-- so that all State objects can use these shared methods.
+---@param state State
+function State:new(state)
+  ---@type State
+  ---@diagnostic disable-next-line
+  self.__index = self
+  setmetatable(state, self)
+  return state
+end
+
+---@param tbl table
+function State:show_out(tbl)
+  local output_buffer = string.format("%s.out", self.file)
+  vim.cmd('silent only') -- Close all windows apart from current
+  vim.cmd('55vsplit')    -- Create new vertical split with 75 columns for new window
+
+  -- Re-use old buffer with same name
+  local bufnr = vim.fn.bufnr(output_buffer, false)
+  if bufnr == -1 then
+    bufnr = vim.api.nvim_create_buf(true, false)
+    vim.api.nvim_buf_set_name(bufnr, output_buffer)
+
+    -- Set buffer options
+    vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+    vim.api.nvim_buf_set_option(bufnr, 'readonly', true)
+  end
+  vim.api.nvim_win_set_buf(0, bufnr)
+
+  -- Coalesce stderr and stdout
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, tbl)
+end
 
 ---@return string filepath
 function Runner:current_file()
@@ -26,11 +60,10 @@ function Runner:state_file()
   return current_file_state
 end
 
----@param state State
-function Runner:write_state_file(state)
+function Runner:write_state_file()
   local state_file = io.open(self:state_file(), "w")
   if state_file then
-    state_file:write(vim.json.encode(state))
+    state_file:write(vim.json.encode(self.state))
     state_file:close()
   else
     -- TODO
@@ -46,7 +79,7 @@ function Runner:try_read_state_file()
       return vim.json.decode(state)
     end)
     if success then
-      return state_tbl
+      return State:new(state_tbl)
     else
       return nil
     end
@@ -59,16 +92,19 @@ function Runner:init()
   if state then
     -- Run the command
     self.state = state
+    self.state = State:new(self.state)
   else
     -- Prompt user for a command to run for the current_file.
     local cmd = vim.fn.input("cmd:", "", "shellcmdline")
-    self.state = { cmd = cmd }
-    self:write_state_file(state)
+    self.state = State:new({
+      file = self:current_file(),
+      cmd = cmd
+    })
+    self:write_state_file()
   end
   self:run()
 end
 
---- @param cmd string
 function Runner:run()
   local cmd = self.state.cmd
   if cmd then
@@ -83,35 +119,16 @@ function Runner:run()
       end,
     })
     vim.fn.jobwait({ job_id })
-    self:on_finish()
+
+    local show = self.err
+    if #show == 1 and show[1] == "" then
+      show = self.out
+    end
+
+    self.state:show_out(show)
   else
     -- TODO maybe do something here
   end
-end
-
-function Runner:on_finish()
-  local output_buffer = string.format("%s.out", self:current_file())
-  vim.cmd('silent only') -- Close all windows apart from current
-  vim.cmd('75vsplit')    -- Create new vertical split with 75 columns for new window
-
-  -- Re-use old buffer with same name
-  local bufnr = vim.fn.bufnr(output_buffer, false)
-  if bufnr == -1 then
-    bufnr = vim.api.nvim_create_buf(true, false)
-    vim.api.nvim_buf_set_name(bufnr, output_buffer)
-
-    -- Set buffer options
-    vim.api.nvim_buf_set_option(0, 'modifiable', false)
-    vim.api.nvim_buf_set_option(0, 'readonly', true)
-  end
-  vim.api.nvim_win_set_buf(0, bufnr)
-  local errors = self.err
-  local output = self.out
-  local replacement = self.err
-  if #replacement == 1 and replacement[1] == "" then 
-    replacement = self.out
-  end
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, replacement)
 end
 
 Runner:init()
